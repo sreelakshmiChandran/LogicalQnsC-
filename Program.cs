f06f4383c79b5bfec32ec95900cf21d829a30b24
Sreelakshmi Chandran <116948144+sreelakshmiChandran@users.noreply.github.com>
2023-01-19 09:28:47 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+Console.WriteLine("Enter the DOB: ");
+var dob = DateTime.Parse(Console.ReadLine());
+
+var today = DateTime.Today;
+Console.WriteLine(today);
+
+TimeSpan interval = dob - today;
+Console.WriteLine("Age in hours: " + interval.Hours);
+Console.WriteLine("Age in days: " + interval.Days);
+Console.WriteLine("Age in weeks: " + (interval.Days / 7));
+Console.WriteLine("Age in months: " + ((interval.Days / 365) * 12));
+Console.WriteLine("Age in years: " + (interval.Days / 365));
+
+var year = dob.Year;
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+{
+    Console.WriteLine("Leap year");
+}
+else
+{
+    Console.WriteLine("Not a leap year");
+}
