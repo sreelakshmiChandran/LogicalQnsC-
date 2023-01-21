dff156cf8230aa1351363ce89fbf2742d899f242
Sreelakshmi Chandran <116948144+sreelakshmiChandran@users.noreply.github.com>
2023-01-21 20:10:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+string[] names = new string[10];
+string[] destinations = new string[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Enter the passenger name: ");
+    names[i] = Console.ReadLine();
+    Console.WriteLine("Enter the desination: ");
+    destinations[i] = Console.ReadLine();
+}
+
+int j = 0;
+int k = 1;
+string[] newArray = new string[20];
+for (int i = 0; i < 10; i++)
+{
+    newArray[j] = names[i];
+    j += 2;
+    newArray[k] = destinations[i];
+    k += 2;
+}
+Console.WriteLine("The names and desinations are:");
+foreach (var item in newArray)
+{
+    Console.WriteLine(item);
+}
